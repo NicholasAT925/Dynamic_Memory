372d149bcece1a09454590fb6704840c09399314
Zuriel <2001555@sit.singaporetech.edu.sg>
2022-05-10 16:06:25 +0800

    Update main.c

---
+++ In_c/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/* Function Declerations */
+
+/* Global Variables */
+
+int main()
+{
+    /* Initializing Global Variables */
+
+    return 0;
+}
+
+/* Function Details */
