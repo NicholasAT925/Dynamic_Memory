adbefa0723b537c5eeb65a5538d19d04508d94d5
Zuriel <2001555@sit.singaporetech.edu.sg>
2022-05-12 21:24:27 +0800

    realloc

--- In_c/main.c
+++ In_c/main.c
@@ -5,11 +5,22 @@
 /* Function Declerations */
 
 /* Global Variables */
+char *str = NULL;
 
-int main()
-{
+int main(){
     /* Initializing Global Variables */
 
+    /* Initial memory allocation */
+    str = (char *) malloc(5 * sizeof(char));
+    strcpy(str, "jason");
+    printf("String = %s, Address of String is = %p\n", str, str);
+
+    /* Reallocating memory */
+    str = (char *)realloc(str, (25 * sizeof(char)));
+    strcat(str, ".com");
+    printf("String = %s, Address of String is = %p\n", str, str);
+
+    free(str);
     return 0;
 }
 
